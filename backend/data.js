cf56eab414f1984ddd313645168e53438b13757e
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2021-12-26 09:53:30 +0200

    created backend

---
+++ backend/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'regular coffee',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 100,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'batch brew'
+        },
+        {
+            _id: '2',
+            name: 'coffee with oat milk',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 100,
+            rating: 5,
+            numReviews: 10,
+            description: 'batch brew'
+        },
+        {
+            _id: '3',
+            name: 'V60',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 100,
+            rating: 3,
+            numReviews: 10,
+            description: 'batch brew'
+        },
+        {
+            _id: '4',
+            name: 'Chemex',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 100,
+            rating: 5,
+            numReviews: 10,
+            description: 'batch brew'
+        },
+        {
+            _id: '5',
+            name: 'americano',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 100,
+            rating: 4,
+            numReviews: 10,
+            description: 'batch brew'
+        },
+        {
+            _id: '6',
+            name: 'Batch brew',
+            category: 'Hot drinks',
+            image: '/images/picture1.jpg',
+            price: '2 euro',
+            countInStock: 0,
+            rating: 3,
+            numReviews: 10,
+            description: 'batch brew'
+        }
+    ]
+}
+
+export default data;
